0d36b240f64051c0dedc9894627597f53ffe326f
fabiorlsouza <fabio.rl.souza@gmail.com>
2020-11-23 20:10:35 -0300

    Commit pelo comando .publicar

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 
 function Home() {
-    return <div>A Neuri é linda e é a melhor esposa do mundo!</div>
+    return <div>A Neuri é linda e é a melhor esposa do mundo!!!</div>
 }
 
 export default Home

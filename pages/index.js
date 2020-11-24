71937c609da86a09445b2668bd75e55085bb38ff
fabiorlsouza <fabio.rl.souza@gmail.com>
2020-11-24 12:10:44 -0300

    Commit pelo comando .publicar

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,44 @@
 
 function Home() {
-    return <div>A Neuri é linda e é a melhor esposa do mundo!</div>
+    return (
+    <div>
+        <html>
+            <head>
+                <title>Site das lojinhas do Abramoff</title>
+            </head>
+            <body>
+            <center><h1>Lojinhas do Abramoff</h1></center>
+            <hr></hr>
+            <p>Bem vindo ao site das lojinhas do Abramoff, onde  nos preocupamos em cuidar de você acima de tudo!</p>
+            <br></br>
+            <hr></hr>
+            <br></br>
+            <h2>Endereço da unidade 1:</h2>
+            Rua blablabla, No 010101 <br></br>
+            Bairro Quebrada do Abramoff - Osasco City - SP<br></br>
+            Brasil - CEP 00000-000<br></br>
+            Telefone: liga pra sua mãe e faça o pedido pra ela!<br></br>
+            <br></br>
+            <br></br>
+            <hr></hr>
+            <br></br>
+            <h2>Endereço da unidade 2:</h2>
+            Rua mehmehmeh, No 101010 <br></br>
+            Bairro Consertada do Abramoff - Osasco City - SP<br></br>
+            Brasil - CEP 11111-111<br></br>
+            Telefone: liga pro seu pai e faça o pedido pra ela!<br></br>
+            <br></br>
+            <br></br>
+            <hr></hr>
+            <br></br>
+            <p> Deus te livre da sua feiura de Osaskense!</p>
+            <p> Todos os direitos reservados.</p>
+            <br></br>
+            
+            </body>
+        </html>
+    </div>
+    )
 }
-
+    
 export default Home
